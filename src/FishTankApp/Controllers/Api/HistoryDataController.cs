25e847d044adc71af60af57d7d05848792fe6d5e
Johnny Oshika <johnnyoshika@gmail.com>
2016-01-22 13:53:51 -0800

    Add HistoryDataController.

---
+++ src/FishTankApp/Controllers/Api/HistoryDataController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using FishTankApp.Services;
+using FishTankApp.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FishTankApp.Controllers.Api
+{
+    public class HistoryDataController : Controller
+    {
+        public HistoryDataController(ISensorDataService sensorDataService)
+        {
+            SensorDataService = sensorDataService;
+        }
+
+        ISensorDataService SensorDataService { get; }
+
+        public IEnumerable<IntHistory> WaterTemperatureHistory()
+        {
+            return SensorDataService.GetWaterTemperatureFahrenheightHistory();
+        }
+
+        public IEnumerable<IntHistory> FishMotionPercentageHistory()
+        {
+            return SensorDataService.GetFishMotionPercentageHistory();
+        }
+
+        public IEnumerable<IntHistory> WaterOpacityPercentageHistory()
+        {
+            return SensorDataService.GetWaterOpacityPercentageHistory();
+        }
+
+        public IEnumerable<IntHistory> LightIntensityLumensHistory()
+        {
+            return SensorDataService.GetLightIntensityLumensHistory();
+        }
+    }
+}
