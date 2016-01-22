25abb3cb8b9d7e5393025ef432a0c49f717ba295
Johnny Oshika <johnnyoshika@gmail.com>
2016-01-22 13:44:09 -0800

    Add HistoryController.

---
+++ src/FishTankApp/Controllers/HistoryController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using FishTankApp.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FishTankApp.Controllers
+{
+    public class HistoryController : Controller
+    {
+        public IActionResult WaterTemperatureChart()
+        {
+            return View("Chart", new ChartViewModel
+            {
+                Title = "Water Temperature",
+                DataUrl = Url.Action("WaterTemperatureHistory", "HistoryData")
+            });
+        }
+
+        public IActionResult FishMotionPercentageChart()
+        {
+            return View("Chart", new ChartViewModel
+            {
+                Title = "Fish Motion Percentage",
+                DataUrl = Url.Action("FishMotionPercentageHistory", "HistoryData")
+            });
+        }
+
+        public IActionResult WaterOpacityPercentageChart()
+        {
+            return View("Chart", new ChartViewModel
+            {
+                Title = "Water Opacity Percentage",
+                DataUrl = Url.Action("WaterOpacityPercentageHistory", "HistoryData")
+            });
+        }
+
+        public IActionResult LightIntensityLumensChart()
+        {
+            return View("Chart", new ChartViewModel
+            {
+                Title = "Light Intensity",
+                DataUrl = Url.Action("LightIntensityLumensHistory", "HistoryData")
+            });
+        }
+    }
+}
---
+++ src/FishTankApp/ViewModels/ChartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishTankApp.ViewModels
+{
+    public class ChartViewModel
+    {
+        public string Title { get; set; }
+        public string DataUrl { get; set; }
+    }
+}
