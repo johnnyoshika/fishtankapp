ab9262b6d66e6e6f7a774cb94fdade2a67d64414
Johnny Oshika <johnnyoshika@gmail.com>
2016-01-22 10:48:14 -0800

    Add ViewModels.

---
+++ src/FishTankApp/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishTankApp.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public SensorTileViewModel WaterTemperatureTile { get; set; }
+        public SensorTileViewModel FishMotionTile { get; set; }
+        public SensorTileViewModel WaterOpacityTile { get; set; }
+        public SensorTileViewModel LightIntensityTile { get; set; }
+    }
+}
---
+++ src/FishTankApp/ViewModels/SensorTileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishTankApp.ViewModels
+{
+    public class SensorTileViewModel
+    {
+        public string Title { get; set; }
+        public int Value { get; set; }
+        public string Url { get; set; }
+        public string IconCssClass { get; set; }
+        public string ColorCssClass { get; set; }
+    }
+}
