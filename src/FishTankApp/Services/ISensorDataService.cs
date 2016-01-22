e1d29465b953525d205c04e2d7ef006ffea1bae1
Johnny Oshika <johnnyoshika@gmail.com>
2016-01-22 10:38:32 -0800

    Implement ISensorDataServices.

--- src/FishTankApp/Services/ISensorDataService.cs
+++ src/FishTankApp/Services/ISensorDataService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FishTankApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,13 @@ namespace FishTankApp.Services
 {
     public interface ISensorDataService
     {
+        IntHistory GetWaterTemperatureFahrenheight();
+        IEnumerable<IntHistory> GetWaterTemperatureFahrenheightHistory();
+        IntHistory GetFishMotionPercentage();
+        IEnumerable<IntHistory> GetFishMotionPercentageHistory();
+        IntHistory GetWaterOpacityPercentage();
+        IEnumerable<IntHistory> GetWaterOpacityPercentageHistory();
+        IntHistory GetLightIntensityLumens();
+        IEnumerable<IntHistory> GetLightIntensityLumensHistory();
     }
 }
--- src/FishTankApp/Services/SensorDataService.cs
+++ src/FishTankApp/Services/SensorDataService.cs
@@ -2,10 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FishTankApp.Models;
 
 namespace FishTankApp.Services
 {
     public class SensorDataService : ISensorDataService
     {
+        IEnumerable<IntHistory> _waterTemperatureHistory;
+        IEnumerable<IntHistory> _fishMotionHistory;
+        IEnumerable<IntHistory> _waterOpacityHistory;
+        IEnumerable<IntHistory> _lightOpacityHistory;
+
+        public IntHistory GetWaterTemperatureFahrenheight()
+        {
+            return GetWaterTemperatureFahrenheightHistory().Last();
+        }
+
+        public IEnumerable<IntHistory> GetWaterTemperatureFahrenheightHistory()
+        {
+            return _waterTemperatureHistory ?? (_waterTemperatureHistory = GetHistory(70, 90));
+        }
+
+        public IntHistory GetFishMotionPercentage()
+        {
+            return GetFishMotionPercentageHistory().Last();
+        }
+
+        public IEnumerable<IntHistory> GetFishMotionPercentageHistory()
+        {
+            return _fishMotionHistory ?? (_fishMotionHistory = GetHistory(30, 50));
+        }
+
+        public IntHistory GetWaterOpacityPercentage()
+        {
+            return GetWaterOpacityPercentageHistory().Last();
+        }
+
+        public IEnumerable<IntHistory> GetWaterOpacityPercentageHistory()
+        {
+            return _waterOpacityHistory ?? (_waterOpacityHistory = GetHistory(10, 20));
+        }
+
+        public IntHistory GetLightIntensityLumens()
+        {
+            return GetLightIntensityLumensHistory().Last();
+        }
+
+        public IEnumerable<IntHistory> GetLightIntensityLumensHistory()
+        {
+            return _lightOpacityHistory ?? (_lightOpacityHistory = GetHistory(200, 220));
+        }
+
+        IEnumerable<IntHistory> GetHistory(int min, int max)
+        {
+            var random = new Random();
+            var result = new List<IntHistory>();
+            for (int i = -10; i < 1; i++)
+                result.Add(new IntHistory(DateTime.Now.AddHours(i), random.Next(min, max)));
+
+            return result;
+        }
     }
 }
